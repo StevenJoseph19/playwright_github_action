fa7a4960fff3a430a4bbd9fbdee4f41e1c6d2ae1
Stephen Samuels <stevesam1927@gmail.com>
2024-07-28 13:14:09 +0530

    first commit

---
+++ tests/github-actions/_1github-change-user-profile-name.test.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test('Github', async ({ page }) => {
+  await page.goto('https://github.com/');
+
+  await expect(page).toHaveTitle('GitHub: Let’s build from here · GitHub');
+
+  await page.getByRole('link', { name: 'Sign in' }).click();
+
+  await page.locator('#login_field').fill('StevenJoseph19');
+
+  await page.fill('#password', 'Jim2719$$**');
+
+  await page.click("input[value='Sign in']");
+
+  await page.click('span.Button-label > img.avatar');
+
+  await page.waitForTimeout(2000);
+
+  await page.getByText('Your profile').click();
+
+  await page.getByText('Edit profile').click();
+
+  await page.waitForTimeout(2000);
+
+  await page.fill('#user_profile_name', 'Stephen Samuels');
+
+  await page.click('span.Button-label:has-text("Save")');
+
+  await page.waitForTimeout(2000);
+});
